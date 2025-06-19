79f64d2472651a4f5dc024409bf345cebad094d5
dani-agudelo <daniiagudelom@gmail.com>
2025-06-18 22:09:39 -0500

    promesas

--- 02-intro-javascript/src/bases/08-funciones-arr.js
+++ 02-intro-javascript/src/bases/08-funciones-arr.js
@@ -1,1 +1,18 @@
-import {} from './../data/heroes';
+// Si se exporta por defecto, se puede importar con cualquier nombre y sin llaves
+// Si se exporta con nombre, se debe importar con el mismo nombre y entre llaves
+
+import heroes, { owners } from "./../data/heroes";
+// console.log("Owners:", owners);
+
+export const getHeroById = (id) => {
+  return heroes.find((hero) => hero.id === id);
+};
+
+const heroe = getHeroById(2);
+// console.log(heroe); // { id: 2, name: 'Spiderman', owner: 'Marvel' }
+
+const getHeroesByOwner = (owner) => {
+  return heroes.filter((hero) => hero.owner === owner);
+};
+const heroesByDC = getHeroesByOwner("DC");
+// console.log(heroesByDC); 
--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,15 +1,38 @@
-import {heroes} from './data/heroes';
+import { getHeroById } from "./bases/08-funciones-arr";
 
+// const promesa = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     const heroe = getHeroById(2);
+//     console.log("2 segundos después");
+//     // resolve(heroe);
+//     reject("No se pudo encontrar el héroe con el ID 2");
+//   }, 2000);
+// });
 
-const getHeroById = (id) => {
-    return heroes.find(hero => hero.id === id);
-}
+// // el then se ejecuta cuando la promesa se resuelve
+// promesa
+//   .then((heroe) => {
+//     console.log("Then de la promesa");
+//     console.log(heroe);
+//   })
+//   .catch((error) => {
+//     console.error("Error en la promesa:", error);
+//   });
 
-const heroe= getHeroById(2);
-console.log(heroe); // { id: 2, name: 'Spiderman', owner: 'Marvel' }
+const getHeroByIdAsync = (id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const heroe = getHeroById(id);
+      if (!heroe) {
+        reject("No se pudo encontrar el héroe con el ID " + id);
+        return;
+      }
+      resolve(heroe);
+    }, 2000);
+  });
+};
 
-const getHeroesByOwner = (owner) => {
-    return heroes.filter(hero => hero.owner === owner);
-}
-const heroesByDC = getHeroesByOwner('DC');
-console.log(heroesByDC); // [{ id: 1, name: 'Batman', owner: 'DC' }, { id: 3, name: 'Superman', owner: 'DC' }, { id: 4, name: 'Flash', owner: 'DC' }]
+getHeroByIdAsync(2).then(console.log).catch(console.warn);
+
+// El console.warn recibe el primer argumento y lo muestra como una advertencia en la consola, no hay que usar un callback
+// Asimismo, el console.log recibe el primer argumento y lo muestra como un mensaje normal en la consola, no hay que usar un callback
